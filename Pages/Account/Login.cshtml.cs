dc7b04347ec284075d4f112390374a272146ca4e
Carlos Andres Orozc Illera <carlosandresorozcoillera@gmail.com>
2024-11-07 14:20:38 -0500

    index

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,46 @@
+using Autenticacion.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs;
+
+namespace Autenticacion.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (User.Email == "correo@gmail.com" && User.Password == "123456")
+            {
+                /// creación de los claims
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,User.Email),
+                };
+
+                // se asocian los claims creados a un nombre de código
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                // se agrega la identidad creada al claims principal de la aplicación
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                // se registra exitosamente la autenticación y se crea la cookie en el navegador
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+}
+    
+
